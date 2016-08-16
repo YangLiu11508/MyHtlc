1594480aeb5bdc5d259653f1fac022ac9a33e142
yangliu <2456985172@11.com>
2016-08-16 11:06:50 +0800

    11

---
+++ app/src/main/java/jrjz_project/jrjz/htlc/com/myhtlc/MainActivity.java
@@ -0,0 +1,13 @@
+package jrjz_project.jrjz.htlc.com.myhtlc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
